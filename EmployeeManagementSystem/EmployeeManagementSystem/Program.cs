38e87aaae9b5491724cb11b165a0cd5aa91e6607
yangryu321 <yangryu321@gmail.com>
2022-07-18 15:33:37 -0400

    Added a wwwroot folder

--- EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
@@ -1,6 +1,12 @@
+// Dependency injection container
 var builder = WebApplication.CreateBuilder(args);
+
+//Middlewares 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//static file middleware
+app.UseStaticFiles();
 
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/Process", () => System.Diagnostics.Process.GetCurrentProcess().ProcessName);
 app.Run();
