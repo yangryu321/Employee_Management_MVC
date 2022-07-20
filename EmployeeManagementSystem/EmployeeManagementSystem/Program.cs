ce0389cd71a472958ebe4a97884b68b2609a891f
yangryu321 <yangryu321@gmail.com>
2022-07-20 15:00:23 -0400

    Added MVC service and middleware

--- EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
@@ -1,12 +1,32 @@
 // Dependency injection container
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+
 
 //Middlewares 
 var app = builder.Build();
 
+//Microsoft.AspNetCore.Hosting.IHostingEnvironment env = app.Services.GetService<Microsoft.AspNetCore.Hosting.IHostingEnvironment>(); 
+
+
+//if(env.IsDevelopment())
+//{
+//    app.UseDeveloperExceptionPage();
+//}
 //static file middleware
 app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/Process", () => System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+
+app.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+
+//app.MapGet("/", () => "Hello World!");
+//app.MapGet("/Process", () =>
+//{
+//    throw new Exception("HAHA");
+//    return System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+//});
+
+
 app.Run();
