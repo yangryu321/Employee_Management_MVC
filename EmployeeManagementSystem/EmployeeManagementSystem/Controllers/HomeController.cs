f0c4993ff39aa33e16620203dbcb635e303c73d4
yangryu321 <yangryu321@gmail.com>
2022-07-20 21:02:23 -0400

    Added another action method. Injected Mock Repository class.

--- EmployeeManagementSystem/EmployeeManagementSystem/Controllers/HomeController.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/HomeController.cs
@@ -4,9 +4,20 @@ namespace EmployeeManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public string Try()
+        {
+            return employeeRepository.Get(1).Name.ToString();
         }
     }
 }
