97bff7e4921e532454098acb1fc85d9e4277b9de
yangryu321 <yangryu321@gmail.com>
2022-07-27 15:25:59 -0400

    Added DbContext class

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Models/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
