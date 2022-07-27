cae64535cd757a41ac014c0860a1cb8fe0d688d2
yangryu321 <yangryu321@gmail.com>
2022-07-27 12:36:31 -0400

    Added annotation for Employee

--- EmployeeManagementSystem/EmployeeManagementSystem/Models/Employee.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Models/Employee.cs
@@ -1,10 +1,18 @@
-﻿namespace EmployeeManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementSystem.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name can be no longer than 50 charactors")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email")]
         public string Email { get; set; }
-        public Dpt Department { get; set; }
+        [Required]
+        public Dpt? Department { get; set; }
     }
 }
