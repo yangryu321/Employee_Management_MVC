d66190504292f4f722891e86ac5477bde78c65e4
yangryu321 <yangryu321@gmail.com>
2022-07-30 12:48:54 -0400

    Added EmployeeCreateViewModel

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementSystem.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+   
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name can be no longer than 50 charactors")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email")]
+        public string Email { get; set; }
+        [Required]
+        public Dpt? Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
