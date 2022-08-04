b7ee55d1f2594a1923ca1e6ef0da4182949649d5
yangryu321 <yangryu321@gmail.com>
2022-08-03 20:58:09 -0400

    Added Error controller for handleing incorrect route

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{statuscode}")]
+        public IActionResult Index(int statuscode)
+        {
+            switch(statuscode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the page does not exist";
+                    break;
+            }
+
+            return View("NotFound",statuscode);
+        }
+    }
+}
