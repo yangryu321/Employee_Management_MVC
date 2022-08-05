ec9849832ff8e1b7f78edcadc10ea9df5d822f2d
yangryu321 <yangryu321@gmail.com>
2022-08-05 15:14:56 -0400

    Injected Nlog to use in ErrorController

--- EmployeeManagementSystem/EmployeeManagementSystem/Controllers/ErrorController.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/ErrorController.cs
@@ -2,12 +2,21 @@
 
 namespace EmployeeManagementSystem.Controllers
 {
+    
     public class ErrorController : Controller
     {
-        [Route("/Error/{statuscode}")]
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+            logger.LogDebug(1, "NLog injected into HomeController");
+        }
+        [Route("Error/{statuscode}")]
         public IActionResult Index(int statuscode)
         {
-            switch(statuscode)
+            logger.LogInformation("Hello, this is the index!");
+            switch (statuscode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the page does not exist";
