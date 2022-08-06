d1a6094e931af4af24b3ae8dd366641f5d2eb90a
yangryu321 <yangryu321@gmail.com>
2022-08-06 15:50:28 -0400

    Added Register button to nav menu

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                var result = await userManager.CreateAsync(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    RedirectToAction(actionName: "Index", controllerName: "Home");
+                }
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("",error.Description);
+            }
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace EmployeeManagementSystem.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match, please try again")]
+        public string ConfirmPassword { get; set; }
+    }
+}
