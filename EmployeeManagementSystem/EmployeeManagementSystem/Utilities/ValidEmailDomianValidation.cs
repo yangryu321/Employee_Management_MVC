23d11b69510835ebf54a4add31b757ee72ded461
yangryu321 <yangryu321@gmail.com>
2022-08-11 16:02:19 -0400

    Created ValidEmailDomainValidation class for Custom atrribute

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Utilities/ValidEmailDomianValidation.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeeManagementSystem.Utilities
+{
+    public class ValidEmailDomianValidation : ValidationAttribute
+    {
+        private readonly string alloweddomain;
+
+        public ValidEmailDomianValidation(string alloweddomain)
+        {
+            this.alloweddomain = alloweddomain;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            string[] strs = value.ToString().Split('@');
+
+            if(strs[1].ToUpper() == alloweddomain.ToUpper())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
