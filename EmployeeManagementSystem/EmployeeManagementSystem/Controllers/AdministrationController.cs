fcaa711f44c6505d88ced8ffef6680ea94001948
yangryu321 <yangryu321@gmail.com>
2022-08-14 16:13:23 -0400

    Created Administration controller. Added CreateRole, ListRoles methods.

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/AdministrationController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                var result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                    return RedirectToAction(actionName: "ListRoles", controllerName: "Administration");
+
+                foreach(var error in result.Errors)
+                    ModelState.AddModelError("",error.Description);
+
+            }
+
+            return View();
+        } 
+
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+
+
+
+    }
+}
