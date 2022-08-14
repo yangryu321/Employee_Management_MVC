36576683e66afbd044fa23d8b9829b18a58a7bfb
yangryu321 <yangryu321@gmail.com>
2022-08-14 16:16:50 -0400

    Deleted two LogDebug

--- EmployeeManagementSystem/EmployeeManagementSystem/Controllers/ErrorController.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/ErrorController.cs
@@ -10,16 +10,18 @@ namespace EmployeeManagementSystem.Controllers
         public ErrorController(ILogger<ErrorController> logger)
         {
             this.logger = logger;
-            logger.LogDebug(1, "NLog injected into HomeController");
+           
         }
+
         [Route("Error/{statuscode}")]
         public IActionResult Index(int statuscode)
         {
-            logger.LogInformation("Hello, this is the index!");
+            
             switch (statuscode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the page does not exist";
+                    logger.LogInformation("404 error, page not found");
                     break;
             }
 
