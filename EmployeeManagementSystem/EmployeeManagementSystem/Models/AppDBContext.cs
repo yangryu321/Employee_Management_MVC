5d4d4d31e22b0cedbe2ea9447b7141ff01785dbb
yangryu321 <yangryu321@gmail.com>
2022-08-14 16:11:45 -0400

    Chnaged IdentityUser to ApplicationUser

--- EmployeeManagementSystem/EmployeeManagementSystem/Models/AppDBContext.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Models/AppDBContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagementSystem.Models
 {
-    public class AppDBContext : IdentityDbContext
+    public class AppDBContext : IdentityDbContext<ApplicationUser>
     {
         public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
         {
