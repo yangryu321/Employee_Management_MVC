be0e375515ae9daa172e4d64e76ff58ef5ac8448
yangryu321 <yangryu321@gmail.com>
2022-08-14 16:17:13 -0400

    Added Extend_User migration.

--- EmployeeManagementSystem/EmployeeManagementSystem/Migrations/20220811201611_Extend_User.Designer.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Migrations/20220811201611_Extend_User.Designer.cs
@@ -4,6 +4,7 @@ using EmployeeManagementSystem.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeManagementSystem.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220811201611_Extend_User")]
+    partial class Extend_User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
