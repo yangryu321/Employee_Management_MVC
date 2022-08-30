6e1a45ae010ef218451a42a2dfb8cb1d78590987
yangryu321 <yangryu321@gmail.com>
2022-08-30 15:30:01 -0400

    Created UserRoleIViewModel

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagementSystem.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        //don't need role here cause it's gonna cause
+        //duplication, pass roleId using ViewBag.
+        //public string RoleId { get; set; }
+        public string UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
