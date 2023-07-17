cb5014e002ac818cec78b7cc82bc0adac08bdb62
yangryu321 <yangryu321@gmail.com>
2023-07-17 17:25:50 -0400

    Feature/Added Claim authorization

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/ViewModels/ClaimStore.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace EmployeeManagementSystem.ViewModels
+{
+    public static class ClaimStore
+    {
+        public static List<Claim> AllClaims = new List<Claim>()
+        {
+            new Claim("Delete Role","Delete Role"),
+            new Claim("Edit Role", "Edit ROle"),
+            new Claim("Create Role", "Create Role")
+        };
+
+    }
+}
